485eaab9f54cc5d97477c68937dd1f9f729838db
DESKTOP-KNNQU3F\kamruzzaman <mkz.ice@gmail.com>
2018-01-22 10:16:25 +0600

    navigation update for Responsive

--- src/app/components/layout/layout.module.ts
+++ src/app/components/layout/layout.module.ts
@@ -4,11 +4,13 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 import { PublicLayoutComponent } from './public-layout/public-layout.component';
 import { NavigationModule } from '../navigation/navigation.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
+    MatSidenavModule,
     SharedModule,
     NavigationModule
   ],
--- src/app/components/layout/public-layout/public-layout.component.ts
+++ src/app/components/layout/public-layout/public-layout.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { MatSidenav } from "@angular/material";
+import { ObservableMedia, MediaChange } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-public-layout',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./public-layout.component.scss']
 })
 export class PublicLayoutComponent implements OnInit {
+  sideNavigationNode = "";
+  @ViewChild('sidenav') sidenav: MatSidenav;
+  // @HostListener('window:resize', ['$event'])
+  // onResize(event) {
+  //   if (event.target.innerWidth < 960) {
+  //     this.sidenav.close();
+  //     this.sideNavigationNode = "over";
+  //   } else {
+  //     this.sidenav.open();
+  //     this.sideNavigationNode = "side";
+  //   }
+  // }
 
-  constructor() { }
+  constructor(public media: ObservableMedia) {
+  }
 
   ngOnInit() {
+    this.media.asObservable()
+      .subscribe((change: MediaChange) => {
+        let medis = ['xs', 'sm'];
+        if (medis.indexOf(change.mqAlias) != -1) {
+          this.sidenav.close();
+          this.sideNavigationNode = "over";
+        }else{
+          this.sidenav.open();
+          this.sideNavigationNode = "side";
+        }
+      });
   }
 
 }
